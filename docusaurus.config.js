7fa247d66cd5689420fa2d5d2597136e7c86ee89
ikepon <tatsuyanoheya@gmail.com>
2022-09-07 22:49:48 +0900

    Use yarn and deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,15 +9,16 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/practical-programmer/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ikepon', // Usually your GitHub org/user name.
+  projectName: 'practical-programmer', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
